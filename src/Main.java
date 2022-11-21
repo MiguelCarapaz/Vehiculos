7796980670662298dfbd847404246de454108a78
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2022-11-20 20:38:42 -0500

    Main y clase vehiculos

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int opcion=0;
+        boolean exit=false;
+        Scanner leer = new Scanner(System.in);
+        Vehiculos vehiculo1 = new Vehiculos();
+        Vehiculos vehiculo2 = new Vehiculos();
+        Vehiculos vehiculo3 = new Vehiculos();
+        Vehiculos vehiculo4 = new Vehiculos();
+        Vehiculos vehiculo5 = new Vehiculos();
+
+        while(!exit){
+            System.out.println("1.- Ingresar datos de Vehiculos");
+            System.out.println("2.- Mostar datos de Vehiculos");
+            System.out.println("3.- Salir");
+            System.out.print("Ingrese la opcion: ");
+            opcion = leer.nextInt();
+            switch (opcion){
+                case 1:
+                    vehiculo1.vehiculo1();
+                    vehiculo2.vehiculo2();
+                    vehiculo3.vehiculo3();
+                    vehiculo4.vehiculo4();
+                    vehiculo5.Vehiculo5();
+                    break;
+                case 2:
+                    vehiculo1.mostrar_datos();
+                    vehiculo2.mostrar_datos();
+                    vehiculo3.mostrar_datos();
+                    vehiculo4.mostrar_datos();
+                    break;
+                case 3:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Ingrese un numero que este en el rango de 1 - 3");
+            }
+        }
     }
-
-
 }
--- src/Vehiculos.java
+++ src/Vehiculos.java
@@ -1,4 +1,120 @@
+import java.util.Scanner;
 public class Vehiculos {
+    String marca, placa, tipo;
+    int anio ;
+    public Vehiculos(){
+        marca="";
+        placa="";
+        tipo ="";
+        anio = 0;
+    }
+    public void vehiculo1() {
+        Scanner leer = new Scanner(System.in);
+        Vehiculos Vehiculo1 = new Vehiculos();
+        System.out.println("VEHICULO 1");
+        System.out.print("Ingrese la marca: ");
+        marca = leer.nextLine();
+        System.out.print("Ingrese el tipo de vehiculo: ");
+        tipo = leer.nextLine();
+        System.out.print("Ingrese la placa: ");
+        placa = leer.nextLine();
+        System.out.print("Ingrese el anio: ");
+        anio = leer.nextInt();
+        System.out.println("------------------------------");
+    }
+    public void vehiculo2() {
+        Scanner leer = new Scanner(System.in);
+        Vehiculos vehiculo2 = new Vehiculos();
+        System.out.println("VEHICULO 2");
+        System.out.print("Ingrese la marca: ");
+        marca = leer.nextLine();
+        System.out.print("Ingrese el tipo de vehiculo: ");
+        tipo = leer.nextLine();
+        System.out.print("Ingrese la placa: ");
+        placa = leer.nextLine();
+        System.out.print("Ingrese el anio: ");
+        anio = leer.nextInt();
+        System.out.println("------------------------------");
+    }
+    public void vehiculo3() {
+        Scanner leer = new Scanner(System.in);
+        Vehiculos vehiculo3 = new Vehiculos();
+        System.out.println("VEHICULO 3");
+        System.out.print("Ingrese la marca: ");
+        marca = leer.nextLine();
+        System.out.print("Ingrese el tipo de vehiculo: ");
+        tipo = leer.nextLine();
+        System.out.print("Ingrese la placa: ");
+        placa = leer.nextLine();
+        System.out.print("Ingrese el anio: ");
+        anio = leer.nextInt();
+        System.out.println("------------------------------");
+    }
+    public void vehiculo4() {
+        Scanner leer = new Scanner(System.in);
+        Vehiculos vehiculo4 = new Vehiculos();
+        System.out.println("VEHICULO 4");
+        System.out.print("Ingrese la marca: ");
+        marca = leer.nextLine();
+        System.out.print("Ingrese el tipo de vehiculo: ");
+        tipo = leer.nextLine();
+        System.out.print("Ingrese la placa: ");
+        placa = leer.nextLine();
+        System.out.print("Ingrese el anio: ");
+        anio = leer.nextInt();
+        System.out.println("------------------------------");
+    }
+    public void Vehiculo5() {
+        Scanner leer = new Scanner(System.in);
+        Vehiculos vehiculo5 = new Vehiculos();
+        System.out.println("VEHICULO 5");
+        System.out.print("Ingrese la marca: ");
+        marca = leer.nextLine();
+        System.out.print("Ingrese el tipo de vehiculo: ");
+        tipo = leer.nextLine();
+        System.out.print("Ingrese la placa: ");
+        placa = leer.nextLine();
+        System.out.print("Ingrese el anio: ");
+        anio = leer.nextInt();
+        System.out.println("------------------------------");
+    }
+    public void mostrar_datos(){
+        System.out.println("Marca: " + getMarca());
+        System.out.println("Tipo: " + getTipo());
+        System.out.println("Placa: " + getPlaca());
+        System.out.println("Anio: " + getAnio());
+        System.out.println("------------------------------");
+    }
+    public String getMarca() {
+        return marca;
+    }
 
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
 }
+
